e040c22ecdd418cd950d03de35052b257a2970fc
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2024-11-12 23:34:58 +0200

    update

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const combinedString = firstArray.join('') + secondArray.join('');
+  if (combinedString.length > maxLength) {
+    return combinedString.slice(0, maxLength);
+  }
+  
+  return combinedString;
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
